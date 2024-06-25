91563a99bfa251a1184e507829d42bef22bc8693
Parameswari Bala <parameswaribala@gmail.com>
2024-06-25 11:20:25 +0530

    updated

--- ecommercelib/dblib/mysqlconn.py
+++ ecommercelib/dblib/mysqlconn.py
@@ -3,7 +3,19 @@ import sqlite3
 import pymysql
 
 
-class Database():
+# def createconn():
+#     conn = pymysql.connect(host="127.0.0.1", user="root", passwd="password", db="ecommercedb")
+#     return conn
+
+
+class MetaSingleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(MetaSingleton, \
+            cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+class Database(metaclass=MetaSingleton):
     connection = None
     def connect(self):
         if self.connection is None:
